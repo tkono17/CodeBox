74d09f2a5788e16bd21c76cd09042531bcab013d
tkono17 <takanori.kono@gmail.com>
2021-11-09 17:01:19 +0900

    Add functions

--- LabProjects/2021A/acceptance.cxx
+++ LabProjects/2021A/acceptance.cxx
@@ -1,6 +1,24 @@
 /*
   acceptance.cxx
 */
+#include <iostream>
+#include <cmath>
+
+void generatePoint(double& x, double& y) {
+  x = -100.0;
+  y = -100.0;
+}
+
+void generateAngles(double& theta, double& phi) {
+  theta = -100.0;
+  phi = -100.0;
+}
+
+bool intersectionAtZ0(double x, double y, double theta, double phi, double d,
+		      double& x0, double& y0) {
+  bool hit=false;
+  return hit;
+}
 
 int main(int argc, char* argv[]) {
   int npoints=10000;
